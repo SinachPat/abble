c717cf187db1baa0477bb9953a1315e4125005ff
SinachPat <sinachpat@gmail.com>
2025-11-12 11:57:59 +0100

    updated theme

--- abble/src/components/signin.vue
+++ abble/src/components/signin.vue
@@ -1,8 +1,8 @@
 <script setup>
-import logo from '@/assets/logo/Abble.png'
+import logo from '@/assets/logo/Abble-logo-dark.png'
 </script>
 <template>
-    <section class="min-h-screen flex flex-col items-center flex-start pt-24 bg-gray-100">
+    <section class="min-h-screen flex flex-col items-center flex-start pt-24 bg-slate-950">
       <!-- Logo -->
       <img v-bind:src="logo" alt="abble logo" class="w-auto h-6 mb-10" />
   
@@ -10,7 +10,7 @@ import logo from '@/assets/logo/Abble.png'
       <div class="flex flex-col items-center space-y-4 w-full max-w-sm">
   
         <!-- Login title -->
-        <h2 class="text-xl font-medium text-gray-900 mb-2">
+        <h2 class="text-xl font-medium text-gray-200 mb-2">
           Log in to Abble
         </h2>
   
@@ -28,9 +28,9 @@ import logo from '@/assets/logo/Abble.png'
   
         <!-- GitHub button -->
         <button
-          class="w-80 flex items-center justify-center gap-2 border border-gray-300 hover:bg-gray-50 py-2 rounded-lg font-medium transition text-gray-700"
+          class="group w-80 flex items-center justify-center gap-2 border border-gray-300 hover:bg-gray-50 hover:text-gray-900 py-2 rounded-lg font-medium transition text-gray-200"
         >
-          <span class="pi pi-github text-gray-700"></span>
+          <i class="pi pi-github text-gray-200 group-hover:text-gray-900"></i>
           Continue with GitHub
         </button>
       </div>
